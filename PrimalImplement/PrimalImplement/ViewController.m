2903eb566d3028eb9225b8509bfe3c5413d5e55f
hayangtze <1604105100@qq.com>
2018-03-25 22:54:47 +0800

    add.

--- PrimalImplement/PrimalImplement/ViewController.m
+++ PrimalImplement/PrimalImplement/ViewController.m
@@ -18,12 +18,12 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     
     Person* p = [Person new];
-    //p.name = @"init";
+    p.name = @"init";
     
     [p pi_performSelectorWithArgs:@selector(setName:),@"test_name"];
     
     [p pi_performSelectorWithArgs:@selector(setAge:),25];
-
+    p.age = 26;
 }
 
 
